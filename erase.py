7aeba2886dc205ff1d1b5131751aeef0557d2ad1
Adam DePrince <deprince@googlealumni.com>
2011-02-18 17:18:05 -0500

    Add code to erase quotes from index

--- erase.py
+++ erase.py
@@ -2,4 +2,8 @@
  
 import common 
 
-open_re
+common.open_redis_connection().delete('pending')
+pool, _raw, _polished, _index = common.open_cassandra_connections()
+for cf in ('RawData', 'PolishedData', 'Index'):
+    print "Truncating ", cf 
+    pool.truncate(cf)
