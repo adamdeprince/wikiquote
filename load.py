24557d03d1b5d1e56872a5318654a4f71b1acb2a
Adam DePrince <deprince@googlealumni.com>
2011-02-18 20:47:57 -0500

    Added comments

--- load.py
+++ load.py
@@ -12,6 +12,7 @@ BLOCK_SIZE = 5000
 
 
 class Reader:
+    """Read and splitup a wikiquote dump."""
     def __init__(self):
         self.que = common.BatchQue()
         self.pool, self.raw, _, _ = common.open_cassandra_connections()
@@ -19,46 +20,98 @@ class Reader:
         self.counter = 1 
 
     def occationally_save(self, interval=BLOCK_SIZE):
+        """Called after each update.  Saves every BLOCK_SIZE invocations."""
         self.counter += 1 
         if self.counter > interval:
             self.save()
             self.counter = 0 
 
     def save(self):
+        """Write batched data to Redis and Cassandra."""
         self.raw.send()
         self.que.save()
 
     @staticmethod
     def digest(s):
+        """Create a truncqted 96 bit md5 hash of a string.
+
+        Args:
+          s: String to hash
+          
+        Returns:
+          24 character long base hexdigest of s.
+        """
         return hashlib.md5(s).hexdigest()[:HASH_SIZE]
 
     @staticmethod 
     def lines_from_page(input):
+        """Extract all of the lines from inside a <page></page> pair.
+        
+        Yields:
+          Lines upto and including </page>
+
+        Cavets:
+          <page> and </page> are expected to be on lines by themselves. 
+        """
         while True:
             line = input.next()
             yield line 
             if '</page>' in line:
                 return 
     @staticmethod
+    """For input stream return the hash (key) and body of the next page."""
     def create_page_key_and_body(input):
         data = '\n'.join(Reader.lines_from_page(input))
         return Reader.digest(data), data 
 
     def upload_page(self, key, body ):
+        """Upload a raw block of xml
+
+        Creates a cassandra row indexed on key with the body stored
+        under the column "body."  
+
+        Also inserts key into the Redis que.  
+
+        Args:
+          key: This block's key.  Usuallly the trunctaed md5 from digest.
+          body: The xml of this block
+        """
         self.que.push(key)
         self.raw.insert(key, dict(body=body))
         self.occationally_save()
 
     def split_into_pages(self, input):
+        """Split a large XML file into pages.
+
+        Processes each page with create_page_key_and_body 
+        
+        Args:
+          input: An inter that returns lines from a file.
+                 Ex: iter(sys.stdin)
+
+        Yields:
+          key, body
+
+          key: The partial md5sum from digest
+          body: The entire xml source code of "this" page.
+
+        Cavets:  We take advantage of the fact the wikiquotes dump foramt 
+        places <page> and </page> alone on lines by themselves. 
+        """
         for line in input:
             if '<page>' in line:
                 
                 # Put the line containing <page> back at the front of the
                 # iter before passing to upload_page
                 
-                yield self.create_page_key_and_body(itertools.chain(iter(['<page>']), input))
-
+                yield self.create_page_key_and_body(
+                    itertools.chain(iter(['<page>']), input))
+    
     def run(self, input):
+        """Slurp in a wikimedia file and store its data and keys.
+
+        Example: Reader().run(sys.stdin)
+        """
         try:
             for key, body in self.split_into_pages(input):
                 self.upload_page(key, body)
--- parse_data.py
+++ parse_data.py
@@ -8,8 +8,6 @@ import pycassa
 import re 
 import redis
 import sys
-import xml
-import xml.sax
 import xml.sax.handler
 
 BATCH_SIZE = 500 
@@ -25,23 +23,41 @@ COMMON = set(['', 'the', 'of', 'and', 'to', 'a', 'in', 'for', 'is', 'on',
 
 def dict_merge(*dicts ):
     """dict_merge
-    
-    
-    """
 
-    keys = set()
-    for dict in dicts:
-        map(keys.add, dict.keys())
+    Merge dictionaries of dictionaries.  
+
+    >>> dict_mergre(dict(a={1:1}, b={1:1}),
+                    dict(b={2:1}, c={2:1}))
+    {'a':{1:1}, 'b':{1:1, 2:1}, 'c':{2:1}}
+
+    Args:
+      *dicts: Dictionaries to merge
+      
+    Returns:
+      Single merged dictionary
+    """
+    keys = reduce(set.union, map(set, dicts))
 
     merged = {}
     for key in keys:
         merged[key] = {}
-        for dict in dicts:
-            merged[key].update(dict.get(key,{}))
+        for d in dicts:
+            merged[key].update(d.get(key,{}))
     return merged
 
-def extract_words(s):
-    for word in s.split():
+def extract_words(wikiwiki_text):
+    """Extract words from a noisy segment of wikiwiki text.
+    
+    >>> list(extract_words('**Hello World!**'))
+    ['hello', 'world']
+    
+    Args:
+      wikiwiki_text: Wikiwiki style text
+    Returns:
+      List of keywords
+    """
+
+    for word in wikiwiki_text.split():
         match = RE_LETTERS_ONLY.search(word.lower())
         if match:
             word = match.groups()[0]
@@ -49,13 +65,25 @@ def extract_words(s):
                 yield word 
 
 class IgnoreErrors(xml.sax.handler.ErrorHandler):
+    """Ignore all ignorable XML parsing errors.""" 
     def error(self, exception):
         pass
-
     def warning(self, exception):
         pass
 
 class WikiCommentHandler(xml.sax.handler.ContentHandler):
+    """Make XML text segments indexable
+
+    >>> article_text = "<a>Hello<b>World</b></a>"
+    >>> handler = WikiCommentHandler()
+    >>> xml.sax.parseString(article_text, handler, IgnoreErrors())
+    >>> handler['a']
+    Hello
+    >>> handler['a-b']
+    World
+
+    Also provides as_dict() method for generating cassandra payloads 
+    """
     def __init__(self, *args, **kwargs):
         xml.sax.handler.ContentHandler.__init__(self, *args, **kwargs)
         self.character_data = []
@@ -70,22 +98,59 @@ class WikiCommentHandler(xml.sax.handler.ContentHandler):
         self.parse_level.append(name)
 
     def endElement(self, name):
-        self.text_data.setdefault('-'.join(self.parse_level), []).append(''.join(self.character_data))
+        current_list = self.text_data.setdefault('-'.join(self.parse_level), [])
+        current_list.append(''.join(self.character_data))
         self.parse_level.pop()
 
     def __getitem__(self, key):
         return self.text_data[key][0].encode('UTF8')
 
     @staticmethod
+    """Writes an integer as an 8 letter string.
+
+    >>> as_ident_string(1)
+    00000001 
+    """
     def as_ident_string(i):
         return "%08d" % int(i)
 
+    """Returns the current parsed enwikiquote <page> segment as a
+    cassandra parsable field. 
+    
+    Returns:
+      dict(text= ...       # Body of the article
+           ident = 0000... # Article ID number 
+           title = ...     # Article title 
+    """
     def as_dict(self):
         return dict(text=self['page'],
                     ident = self.as_ident_string(self['page-id']),
                     title = self['page-title'])
 
+
 def parse_article(article_text):
+    """Parse an article.
+
+    Given an aritcle's XML text erturns the id number, its body and a
+    transposes list of keywords sutiable for cassandra.
+
+    Args:
+      article_text: XML of the wikiquotes page 
+    
+    Returns:
+      id, data, words
+
+      id: The article's id # for use as a cassandra key 
+      data: text, ident and title from WikiCommentHandler.as_dict
+
+      words: A transposed text index with keywords as rows markers and
+        article ids as columns.
+
+    Cavets: Note this doesn't scale very well in the long run,
+        cassandra's ram requirements are proportional to the number of
+        columns, and this creates a column for each document, but for
+        now, it works.)
+    """
     handler = WikiCommentHandler()
     xml.sax.parseString(article_text, handler, IgnoreErrors())
 
@@ -98,8 +163,27 @@ def parse_article(article_text):
     return data['ident'], data, words
 
 
-
 def process_one_element(que, pool, raw, polished ):
+    """Process the next item in the Redis que
+    
+    Args:
+      que: common.Que object holding list of raw file ids to parse
+      pool: Cassandra pool
+      raw: Cassandra ColumnStore from which to retrieve raw data
+      polished: Cassandra Column store to write parsed data.  This may
+        be a batch object.
+
+    Returns:
+      Keyword to document id matching
+
+    Notes:
+
+      There is a lot of keyword overlap between articles, so instead
+      of forciing cassandra to notice that we updating the same rows
+      over and over again, these items are returned so that the
+      connsoluation can be performed in python on an "interarticle"
+      basis.
+    """
     md5 = None
     try:
         while True:
@@ -109,15 +193,24 @@ def process_one_element(que, pool, raw, polished ):
             key, polished_data, keywords = parse_article(data)
             polished.insert(key, polished_data)
             return keywords
-
-    except KeyboardInterrupt, e:
+    except KeyboardInterrupt:
         if md5: que.unpop(md5)
         return None
-    except Exception, e:
+    except Exception, exc:
         if md5: que.push(md5)
-        raise e 
+        raise exc 
 
 def process(status=lambda x:None):
+
+    """Process all of the outstanding raw messages
+
+    Args: 
+      status: Function that accepts a string to report status.  Right
+        now "status" is just "." for each processed item and "!" for
+        each batched save.
+    Todo:
+      We shuld be using multigets to reduce the load on cassandra.  
+    """
     q = common.Que()
     pool, raw, polished, index = common.open_cassandra_connections()
     polished = polished.batch()
@@ -144,10 +237,10 @@ def process(status=lambda x:None):
         status('done\n')
     except KeyboardInterrupt:
         polished.send()
-        
+
+
 if __name__ == "__main__":
     def log(s):
         sys.stdout.write(s)
         sys.stdout.flush()
-
     process(status=log)
