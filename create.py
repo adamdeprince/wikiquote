b41ee65686c2d78349cc5b30301d61933f4af54b
Adam DePrince <deprince@googlealumni.com>
2011-02-18 21:52:24 -0500

    Nope, it didn't work

--- create.py
+++ create.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
+from pycassa import SystemManager 
 
 sys = SystemManager('127.0.0.1:9160')
 sys.create_keyspace('Articles', replication_factor=1)
 for cf in ['RawData', 'PolishedData', 'index']:
-    sys.create_column_family('TestKeyspace', cf)
+    sys.create_column_family('Articles', cf)
 
